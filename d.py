824db15dae345102e4ef6a33013193eb299acf76
Megartron <148366143+Megartron@users.noreply.github.com>
2024-11-05 19:03:19 +0100

    Add files via upload

--- d.py
+++ d.py
@@ -11,30 +11,17 @@ with open("iris_dataset.csv", newline="") as iris_dataset:
         iris_list.append(", ".join(row))
 
 
-x = []
-y = []
+x_length = []
+y_width = []
+y_petal_length = []
+y_petal_width = []
 
-for i in iris_list[1:10]:
+for i in iris_list[1:30]:
     print(i)
-    x.append(float(i[0:3]))
-    y.append(float(i[5:8]))
-
-
-
-def f(x, m):
-    return m * x
-
-m = 0.65
-
-
-
-
-
-
-
-print("x: ",x)
-print(y)
-
+    x_length.append(float(i[0:3]))
+    y_width.append(float(i[5:8]))
+    y_petal_length.append(float(i[10:13]))
+    y_petal_width.append(float(i[15:18]))
 
 
 
@@ -43,51 +30,64 @@ def residum(m, werte_x, werte_y):
     residum_summe = 0
     for n in range(len(werte_x)):
         residum_summe += (werte_y[n] - m * werte_x[n])**2
-    print(residum_summe)
     return residum_summe
 
 
-
-aktuelle_steigung = 1
-aktuelle_residuum = 0
-
-neue_residuum_plus = 0
-neue_residuum_minus = 0
-neue_residuum = 0
-
-steigung_anderung = 1
-
-for i in range(100):
-    aktuelle_residuum = residum(aktuelle_steigung, x, y)
-    neue_residuum_minus = residum(aktuelle_steigung - steigung_anderung, x, y)
-    neue_residuum_plus = residum(aktuelle_steigung + steigung_anderung, x, y)
-    neue_residuum = neue_residuum_plus if neue_residuum_plus < neue_residuum_minus else neue_residuum_minus
-
-    if neue_residuum < aktuelle_residuum:
-        aktuelle_steigung = aktuelle_steigung + steigung_anderung if neue_residuum_plus < neue_residuum_minus else aktuelle_steigung - steigung_anderung
-        print("NEUE RESIDUUM: ", neue_residuum)
-    else:
-        steigung_anderung = steigung_anderung*0.1
-
-print("STEIGUNG: ", aktuelle_steigung)
-
-
-aktuelle_steigung = 0.68
-
-    #print(residum(x[n], y[n], m))
-    #residum_x.append(y[n])
-
-gerade_x = [0,4, 6]
-greade_y = [0,f(4.1, aktuelle_steigung), f(6.2, aktuelle_steigung)]
+#steigungen = []
+#residuen = []
 
 
 
-plt.plot(gerade_x, greade_y)
-plt.plot([2,10], [2, 10], "bo")
-plt.plot(x, y, "ro")
+def steigung_berechnen(x, y):
+    aktuelle_steigung = 1
+    aktuelle_residuum = 0
+
+    neue_residuum_plus = 0
+    neue_residuum_minus = 0
+    neue_residuum = 0
+
+    steigung_anderung = 1
+
+    for i in range(100):
+        aktuelle_residuum = residum(aktuelle_steigung, x, y)
+        neue_residuum_minus = residum(aktuelle_steigung - steigung_anderung, x, y)
+        neue_residuum_plus = residum(aktuelle_steigung + steigung_anderung, x, y)
+        neue_residuum = neue_residuum_plus if neue_residuum_plus < neue_residuum_minus else neue_residuum_minus
+
+        if neue_residuum < aktuelle_residuum:
+            aktuelle_steigung = aktuelle_steigung + steigung_anderung if neue_residuum_plus < neue_residuum_minus else aktuelle_steigung - steigung_anderung
+            #steigungen.append(aktuelle_steigung)
+            #residuen.append(neue_residuum)
+            print("NEUE RESIDUUM: ", neue_residuum)
+        else:
+            steigung_anderung = steigung_anderung*0.1
+    
+    print("STEIGUNG: ", aktuelle_steigung)
+    return aktuelle_steigung
+
+
+steigung = steigung_berechnen(x_length, y_width)
+steigung2 = steigung_berechnen(x_length, y_petal_length)
+steigung3 = steigung_berechnen(x_length, y_petal_width)
+
+gerade_x_length = np.linspace(0, 6.5, 10)
+
+greade_y = steigung * gerade_x_length
+greade_y_petal_length = steigung2 * gerade_x_length
+gerade_y_petal_width = steigung3 + gerade_x_length
+print(gerade_y_petal_width)
+
+#plt.plot(steigungen, residuen, "bo")
+plt.plot(gerade_x_length, greade_y)
+plt.plot(gerade_x_length, greade_y_petal_length)
+plt.plot(gerade_x_length, gerade_y_petal_width)
+
+plt.plot(x_length, y_petal_width, "bo")
+plt.plot(x_length, y_petal_length, "go")
+plt.plot(x_length, y_width, "ro")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
-#residuum durchschnitt berechnen
 
+#TODO Gradientenverfahren
