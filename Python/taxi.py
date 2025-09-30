20faf2068570c0ce87347c17ba336d4fdd5dbeb4
Megartron <148366143+Megartron@users.noreply.github.com>
2025-09-30 16:46:02 +0200

    Update taxi.py

--- Python/taxi.py
+++ Python/taxi.py
@@ -1,97 +1,243 @@
 import tkinter
-import random;
+import random
+import math
 
 class Taxi():
     def __init__(self, root):
         self.root = root
-        self.canvas = tkinter.Canvas(root, width=520, height=520)
+
+        self.canvas = tkinter.Canvas(root, width=520, height=520, bg="black")
+        # main variables
         self.felder = ((" " for _ in range(5)) for _ in range(5))
         self.sonderfelder = ((0, 1), (1, 4), (3, 0), (4, 2))
-        self.fahrgast_feld = ()
-        self.aufgenommen = False
-        self.felder_koordinaten = {}
-        self.feld = ()
-        self.einstellungen = (random.randint(0, 4), random.randint(0, 3)) # fahrgast, ziel
-
-        self.anzahl_züge = 0
-        self.code
+        self.taxi_bewegung = (0, 0)
+        self.moglichkeiten = ("oben", "unten", "links", "rechts")
+        self.fahrgast = ()
+        self.ziel = ()
+        self.taxi_pos = ()
+        self.taxi_pos_px = ()
+        self.aufgenommen = 0
         self.map = [[] for _ in range(500)]
+        self.anzahl_züge = 0
+        self.code = 0
+        self.taxi_gast_code = 0
+        self.ziel_code = 0
+        self.current_round = []
 
-        self.bewegungen = ("up", "down", "left", "right")
+        # helping variables
+        self.dauer = 700
+        self.delta_dis = 0
 
-        self.spielfeld_zeichnen()
-        self.felder_markieren()
-        self.spawn_taxi()
-        
+        self.felder_koordinaten = {}
+        self.animated = False
+        self.while_animation = False
+        self.vorgegebener_weg = ["links", "oben", "rechts", "oben"]
+
+        self.create_widgets()
+        self.zurucksetzen()
+
         self.canvas.pack(expand=True)
+        self.tasten_initiieren()
 
-        self.main_loop()
+    # Animation, Grafiks
+    def taxi_animation(self, start_x: int, start_y):
+        v = 50/self.dauer
+        self.taxi_pos_px = (start_x + self.taxi_bewegung[0] * v, start_y + self.taxi_bewegung[1] * v)
 
+        if (self.delta_dis + abs(self.taxi_bewegung[0] * v) + abs(self.taxi_bewegung[1] * v) >= 100):
+            self.delta_dis = 0
+            self.taxi_pos_px = self.get_coords(self.taxi_pos)
+        else:
+            self.delta_dis += (abs(self.taxi_bewegung[0] * v) + abs(self.taxi_bewegung[1] * v))
+
+        self.taxi_zeichnen(self.taxi_pos_px, "converted")
+    
+    def set_animation(self, value) -> None:
+        if (self.while_animation):
+            return
+        self.dauer = int(value)
+
+    def create_widgets(self) -> None:
+        scale_animation = tkinter.Scale(self.root, from_=10, to=1000, orient="horizontal", label="Animation speed", 
+                                        command=self.set_animation)
+        scale_animation.pack()
+
+    def taxi_zeichnen(self, coords: tuple, state: str) -> None:
+        if state == "converted":
+            x, y = coords
+        else:
+            x, y = self.get_coords(coords)
+
+        self.canvas.create_rectangle(x - 37, y - 10, x + 37, y + 10, fill="yellow", outline="yellow")
+
+        self.canvas.create_polygon((x - 30, y - 9, x - 15, y - 26, x - 15, y - 9), fill="#4cd5ff", outline="yellow", width=2)
+        self.canvas.create_rectangle(x - 15, y - 25, x + 8, y - 9, fill="#4cd5ff", outline="yellow", width=2)
+        self.canvas.create_polygon((x + 8, y - 9, x + 8, y - 25, x + 23, y - 9), fill="#4cd5ff", outline="yellow", width=2)
+
+        self.kreis_zeichnen(x - 19, y + 13, 16, "black")
+        self.kreis_zeichnen(x + 15, y + 13, 16, "black")
+        self.kreis_zeichnen(x - 19, y + 13, 12, "grey")
+        self.kreis_zeichnen(x + 15, y + 13, 12, "grey")
+        self.kreis_zeichnen(x - 19, y + 13, 2, "black")
+        self.kreis_zeichnen(x + 15, y + 13, 2, "black")
+
+    # Spielfeld erschaffen
     def spielfeld_zeichnen(self) -> None:
         for i in range(6):
             x = 10 + i * 100
             y = 10 + i * 100
-            self.canvas.create_line(x, 10, x, 510)
-            self.canvas.create_line(10, y, 510, y)
+            self.canvas.create_line(x, 10, x, 510, fill="white")
+            self.canvas.create_line(10, y, 510, y, fill="white")
 
-            if i > 5:
-                break
-
-            for j in range(5):
-                self.felder_koordinaten[(i, j)] = (60 + j * 100, 60 + i * 100)
-
-    def new_move(self, richtung: str):
-        if (len(self.map[self.code]) < self.anzahl_züge):
-            self.map[self.code].append({
-                "up": 100,
-                "down": 100,
-                "left": 100,
-                "right": 100
-            })
-        
-        self.map[self.code][self.anzahl_züge][richtung] -= self.anzahl_züge
-        
-    def kodieren(self, coords: tuple, einstellungen: tuple) -> int:
-        return ((5 * coords[0] + coords[1]) * 5 + einstellungen[0]) * 4 + einstellungen[1]
+            if i < 5:
+                for j in range(5):
+                    self.felder_koordinaten[(i, j)] = (60 + j * 100, 60 + i * 100)
     
-    def move(self, richtung: str) -> bool:
-        if (richtung == "up"):
-            if (self.feld[1] - 1 < 0):
-                return False
-            self.feld = (self.feld[0], self.feld[1] - 1)
+    def felder_markieren(self) -> None:
+        for coords in self.sonderfelder:
+            x, y = self.get_coords(coords)
+            self.fill_field(x, y, "red")
 
-        if (richtung == "down"):
-            if (self.feld[1] + 1 > 4):
-                return False
-            self.feld = (self.feld[0], self.feld[1] + 1)
+    def tasten_initiieren(self) -> None:
+        self.root.bind("w", lambda x: self.taxi_bewegen("oben"))
+        self.root.bind("a", lambda x: self.taxi_bewegen("links"))
+        self.root.bind("s", lambda x: self.taxi_bewegen("unten"))
+        self.root.bind("d", lambda x: self.taxi_bewegen("rechts"))
 
-        if (richtung == "left"):
-            if (self.feld[0] - 1 < 0):
-                return False
-            self.feld = (self.feld[0] - 1, self.feld[1])
+    def init_ziel_gast(self) -> None:
+        ziel = random.randint(0, 3)
+        gast = random.randint(0, 4)
+        while (gast == ziel):
+                gast = random.randint(0, 4)
+        
+        self.ziel = self.sonderfelder[ziel]
+        if (gast == 4):
+            self.fahrgast = self.taxi_pos
+        else:
+            self.fahrgast = self.sonderfelder[gast]
+        
+        self.taxi_gast_code = gast
+        self.ziel_code = ziel
 
-        if (richtung == "right"):
-            if (self.feld[0] + 1 > 4):
-                return False
-            self.feld = (self.feld[0] + 1, self.feld[1])
-
-    def main_loop(self):
-        self.canvas.delete("all")
-        x, y = self.get_coords(self.feld)
-        self.canvas.create_rectangle(x - 10, y - 15, x + 10, y + 15, fill="yellow")
-
+    def visuals(self) -> None:
         self.spielfeld_zeichnen()
         self.felder_markieren()
 
-        richtung = random.choice(self.bewegungen)
-        self.move(richtung)
+        x, y = self.get_coords(self.ziel)
+        self.fill_field(x, y, "green")
 
-        if (self.fahrgast_feld == self.feld):
-            self.aufgenommen = True
+        # Taxi
+        if self.animated:
+            x0, y0 = self.taxi_pos_px
+            x1, y1 = self.get_coords(self.taxi_pos)
+            
+            if math.sqrt((x0 - x1)**2 + (y0 - y1)**2) <= 3:
+                self.taxi_bewegung = (0, 0)
+                self.taxi_zeichnen(self.taxi_pos, "not converted")
+                self.taxi_pos_px = self.get_coords(self.taxi_pos)
+                self.while_animation = False
+            else:
+                self.delta_dis = 0
+                self.while_animation = True
+                sx, sy = self.taxi_pos_px
+                self.taxi_animation(sx, sy)
+        else:
+            self.taxi_zeichnen(self.taxi_pos, "not converted")
+            self.taxi_pos_px = self.get_coords(self.taxi_pos)
 
-        self.root.after(250, self.main_loop)
+        # Fahrgast, Ziel
+        x, y = self.taxi_pos_px if self.aufgenommen > 0 else self.get_coords(self.fahrgast)
+        self.kreis_zeichnen(x, y, 10, "blue")     
 
-    def dekodieren(code: int) -> tuple:
+    def set_code(self, code: int):
+        ziel, fahrgast_pos, zeile, spalte = self.dekodieren(code)
+
+        print((ziel, fahrgast_pos, zeile, spalte))
+
+        self.taxi_pos = (spalte, zeile)
+        self.ziel_code = ziel
+        self.taxi_gast_code = fahrgast_pos
+
+        self.ziel = self.sonderfelder[ziel]
+        if (fahrgast_pos == 4):
+            self.fahrgast = self.taxi_pos
+        else:
+            self.fahrgast = self.sonderfelder[fahrgast_pos]
+
+    # Helpers
+    def zurucksetzen(self) -> None:
+        self.spielfeld_zeichnen()
+        self.felder_markieren()
+        self.taxi_erschaffen()
+        self.init_ziel_gast()
+        self.code = self.kodieren(self.taxi_pos, (self.taxi_gast_code, self.ziel_code))
+        self.anzahl_züge = 0
+        self.aufgenommen = 0
+    
+    def quadratische_liste(self, n: int) -> list:
+        a = 5
+        e = 100
+        schritte = [(i / (n - 1))**2 for i in range(n)]
+        werte = [a + (e - a) * s for s in schritte]
+        return werte
+
+    def kreis_zeichnen(self, x: int, y: int, r: int, color: str) -> None:
+        x0 = x - r
+        y0 = y - r
+        x1 = x + r
+        y1 = y + r
+        return self.canvas.create_oval(x0, y0, x1, y1, fill = color)
+
+    def get_coords(self, coords: tuple) -> tuple:
+        return self.felder_koordinaten[coords]
+    
+    def fill_field(self, x, y, color: str) -> None:
+        x0 = x - 50
+        y0 = y - 50
+        x1 = x + 50
+        y1 = y + 50
+        self.canvas.create_rectangle(x0, y0, x1, y1, outline="white", fill=color)
+
+    def taxi_erschaffen(self) -> None:
+        feld = (random.randint(0, 4), random.randint(0, 4))
+        self.taxi_pos_px = self.get_coords(feld)
+        self.taxi_pos = feld
+    
+    # Logik
+    def taxi_bewegen(self, richtung: str) -> None:
+        if self.while_animation:
+            return
+        if richtung == "rechts":
+            if self.taxi_pos[1] + 1 > 4:
+                return
+            self.taxi_bewegung = (100, 0)
+            self.taxi_pos = (self.taxi_pos[0], self.taxi_pos[1] + 1)
+        
+        elif richtung == "links":
+            if self.taxi_pos[1] - 1 < 0:
+                return
+            self.taxi_bewegung = (-100, 0)
+            self.taxi_pos = (self.taxi_pos[0], self.taxi_pos[1] + -1)
+        
+        elif richtung == "unten":
+            if self.taxi_pos[0] + 1 > 4:
+                return
+            self.taxi_bewegung = (0, 100)
+            self.taxi_pos = (self.taxi_pos[0] + 1, self.taxi_pos[1])
+
+        elif richtung == "oben":
+            if self.taxi_pos[0] - 1 < 0:
+                return
+            self.taxi_bewegung = (0, -100)
+            self.taxi_pos = (self.taxi_pos[0] - 1, self.taxi_pos[1])
+
+        else:
+            raise Exception("Ungultige Bewegungseingabe!")
+
+    def kodieren(self, coords: tuple, einstellungen: tuple) -> int:
+        return ((5 * coords[0] + coords[1]) * 5 + einstellungen[0]) * 4 + einstellungen[1]
+
+    def dekodieren(self, code: int) -> tuple:
         ziel = code % 4
         code //= 4
 
@@ -103,55 +249,67 @@ class Taxi():
 
         return (ziel, fahrgast_pos, zeile, spalte)
 
-    def get_coords(self, coords: tuple) -> tuple:
-        return self.felder_koordinaten[coords]
-    
-    def create_circle(self, x, y, r, color) -> None:
-        x0 = x - r
-        y0 = y - r
-        x1 = x + r
-        y1 = y + r
-        self.canvas.create_oval(x0, y0, x1, y1, fill= color)
+    # Computer
+    def random_move(self) -> str:
+        return random.choice(self.moglichkeiten)
 
-    def felder_markieren(self) -> None:
-        for coords in self.sonderfelder:
-            x, y = self.get_coords(coords)
-            self.create_circle(x, y, 10, "black")
+    def new_move(self):
+        if (len(self.map[self.code]) <= self.anzahl_züge):
+            self.map[self.code].append({
+                "oben": 100,
+                "unten": 100,
+                "links": 100,
+                "rechts": 100
+            })
+    
+    def next_max_reward(self):
+        # TODO
+        ...
+    
+    def auswertung(self, richtung: str, a: int):
+        self.map[self.code][self.anzahl_züge][richtung] -= self.anzahl_züge * a
+
+        l = len(self.map[self.code])
+        if self.aufgenommen == 1:
+            belohnungen = self.quadratische_liste(l)
+            for i in range(l):
+                self.map[self.code][i][self.current_round[i]] += belohnungen[i]
+            self.aufgenommen = 2
+
+
+
+
+    # Main
+    def update(self) -> None:
+        self.canvas.delete("all")
+        self.visuals()
         
-        x, y = self.get_coords(self.sonderfelder[self.einstellungen[1]])
-        self.create_circle(x, y, 10, "green")
+        
+        zug = self.random_move()
+        self.taxi_bewegen(zug)
 
-        if (self.aufgenommen):
-            self.fahrgast_feld = self.feld
-            x, y = self.get_coords(self.fahrgast_feld)
-            self.create_circle(x, y, 10, "blue")
+        # Kontrolle Fahrgast
+        if (self.taxi_pos == self.fahrgast and self.aufgenommen == 0):
+            self.aufgenommen = 1
 
-        else: 
-            if (self.einstellungen[0] == 4):
-                self.fahrgast_feld = self.feld
-                
+        self.new_move()
+        self.current_round.append(zug)
+        self.auswertung(zug, 0.5)
+        self.anzahl_züge += 1
+        
+        print(self.map[self.code])
+        print("-------------------------------------------------------------")
 
-            elif (self.einstellungen[0] == self.einstellungen[1]):
-                while (self.einstellungen[0] == self.einstellungen[1]):
-                    self.einstellungen[0] = (random.randint(0, 3), self.einstellungen[1])
-                
-                self.fahrgast_feld = self.sonderfelder[self.einstellungen[0]]
+        self.root.after(250, self.update)
 
-            else: 
-                self.fahrgast_feld = self.sonderfelder[self.einstellungen[0]]
-
-            x, y = self.get_coords(self.fahrgast_feld)
-            self.create_circle(x, y, 10, "blue")
     
-    def spawn_taxi(self) -> None:
-        self.feld = (random.randint(0, 4), random.randint(0, 4))
-        x, y = self.get_coords(self.feld)
-        self.canvas.create_rectangle(x - 10, y - 15, x + 10, y + 15, fill="yellow")
-
 
 if __name__ == "__main__":
 
     root = tkinter.Tk()
     taxi1 = Taxi(root)
+
+    taxi1.set_code(143)
+    taxi1.update()
 
     root.mainloop()
